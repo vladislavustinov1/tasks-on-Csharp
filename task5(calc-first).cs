89200aa2adb75f3d8793058871b00e09388918d1
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2023-01-25 21:53:07 +0300

    Add files via upload

---
+++ task5(calc-first).cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            string action;
+            Console.WriteLine("Введите первое число:");
+            firstNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            secondNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите операцию: +, -, /, *");
+            action = Console.ReadLine();
+            switch (action)
+            {
+                default:
+                    { 
+                    Console.WriteLine("Такой операции не существует в данной программе!");
+                    Console.WriteLine("Введите любой символ для выхода из программы:");
+                    Console.ReadLine();
+                    break;
+                    }
+                case "+":
+                    { 
+                    Console.WriteLine("Результат сложения:");
+                    Console.WriteLine(firstNumber + secondNumber);
+                    Console.WriteLine("Введите любой символ для выхода из программы:");
+                    Console.ReadLine();
+                    break;
+                    }
+                case "-":
+                    {
+                        Console.WriteLine("Результат вычитания:");
+                        Console.WriteLine(firstNumber - secondNumber);
+                        Console.WriteLine("Введите любой символ для выхода из программы:");
+                        Console.ReadLine();
+                        break;
+                    }
+                case "/":
+                    {
+                        Console.WriteLine("Результат деления:");
+                        Console.WriteLine(firstNumber / secondNumber);
+                        Console.WriteLine("Введите любой символ для выхода из программы:");
+                        Console.ReadLine();
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.WriteLine("Результат умножения:");
+                        Console.WriteLine(firstNumber * secondNumber);
+                        Console.WriteLine("Введите любой символ для выхода из программы:");
+                        Console.ReadLine();
+                        break;
+                    }
+            }
+
+        }
+    }
+}
---
+++ task5(calc-if).cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc_second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.Clear();
+            double numberFirst, numberSecond;
+            string action;
+            Console.WriteLine("Введите первое число:");
+            numberFirst = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            numberSecond = double.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите операцию +, -, /, *:");
+            action = Console.ReadLine();
+            if (action == "+")
+            {
+                Console.WriteLine("Результат сложения:");
+                Console.WriteLine(numberFirst + numberSecond);
+            } else if (action == "-")
+            {
+                Console.WriteLine("Результат вычитания:");
+                Console.WriteLine(numberFirst - numberSecond);
+            } else if (action == "*")
+            {
+                Console.WriteLine("Результат умножения");
+                Console.WriteLine(numberFirst * numberSecond);
+            } else if (action == "/")
+            {
+                Console.WriteLine("Результат деления:");
+                Console.WriteLine(numberFirst / numberSecond);
+            }
+                Console.WriteLine("Введите любой символ для продолжения");
+                Console.ReadLine();
+            }
+        }
+    }
+}
