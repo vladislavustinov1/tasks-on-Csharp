cc6ebbfa210d75fd494529720c6850277f95935e
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2023-01-25 23:07:10 +0300

    Add files via upload

---
+++ task6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task6
+{
+    class task6
+    {
+        static void Main(string[] args)
+        {
+            uint odd, even;
+            odd = 0;
+            even = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+            int value = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+            while (value <= limit)
+            {
+                if (value % 2 == 0)
+                {
+                    even++;
+                    evenSum += value;
+                } else
+                {
+                    odd++;
+                    oddSum += value;
+                }
+                value++;
+
+            }
+            Console.WriteLine("Кол-во нечётных: " + odd);
+            Console.WriteLine("Кол-во чётных: " + even);
+            Console.WriteLine("Сумма нечётных: " + oddSum);
+            Console.WriteLine("Сумма чётных: " + evenSum);
+        }
+    }
+}
