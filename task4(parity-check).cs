6f59eda9b26a1877519a7cf42cee85f05fa7c2de
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2023-01-25 03:11:41 +0300

    Add files via upload

---
+++ task4(parity-check).cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Введите ваше число:");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число " + number + " чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число " + number + " нечётное");
+            }
+        }
+    }
+}
