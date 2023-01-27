a3f3384e86f3a3d42baba0340ca75cc8f3807b3a
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2023-01-28 01:24:49 +0300

    Add files via upload

---
+++ task7(arr-read-keyboard).cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            int cnt = 0;
+            Console.Write("Кол-во элементов массива: ");
+            int elem = int.Parse(Console.ReadLine());
+            int[] arr = new int [elem];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                cnt++;
+                Console.WriteLine("Введите элемент массива:");
+                arr[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(cnt + "-й элемент массива равен " + arr[i]);
+                if (arr[i] % 2 == 0)
+                {
+                    res += arr[i];
+                }
+            }
+            for (int i = arr.Length - 1; i > 0; --i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n" + "Сумма четных чисел: " + res);
+            var str = string.Join(" ", arr);
+            Console.WriteLine("\n" + "Длина массива: " + arr.Length);
+            Console.WriteLine("Полученный массив: " + str);
+        }
+    }
+}
